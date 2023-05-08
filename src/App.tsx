cb64630590d385266422f8f1e2834c9125756948
victorlouly <91222566+victorlouly@users.noreply.github.com>
2023-05-08 00:44:58 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Router } from './router';
 import { Context, ContextProvider } from './context/Context';
 import io from 'socket.io-client';
 
-const socket = io("https://chatfrontend-eta.vercel.app/");
+const socket = io("https://chatbackend-sgda.onrender.com");
 
 function App() {
 
